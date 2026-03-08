4a31920a2350f48cc2a2eb755ac0a3ec447e6338
Daniel Ruiz <jhafet192@gmail.com>
2026-03-08 10:17:58 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Pensamiento Computacional/Program.cs
@@ -0,0 +1,18 @@
+﻿int opcion;
+do
+{
+    Console.WriteLine("MENU.Opciones:Opcion 1,Opcion 2, Opcion 3,Opcion 4,Opcion 4,Opcion 5,");
+    Console.WriteLine("Opcion 1 : Evaluar nuevo contenido  ");
+    Console.WriteLine("Opcion 2 : Mostrar reglas del sistema ");
+    Console.WriteLine("Opcion 3 : Mostrar estadísticas de la sesión ");
+    Console.WriteLine("Opcion 4 : Reiniciar estadísticas");
+    Console.WriteLine("Opcion 5 : Salir");
+    opcion = int.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            {
+
+            }
+    }
+}
